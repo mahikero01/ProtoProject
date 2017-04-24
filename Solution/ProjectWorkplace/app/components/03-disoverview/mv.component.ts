4e53256d64bee1bd0fef7fef67b5c4ae83d92198
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-04-24 16:52:18 -0400

    up

--- Solution/ProjectWorkplace/app/components/03-disoverview/mv.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-    moduleId: module.id,
-    templateUrl:`mv.component.html`
-})
-export class MVComponent  { 
-    path:string = 'resources/workplace/03 - Mission vision.jpg';
-}
--- Solution/ProjectWorkplace/app/components/03-disoverview/org.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-    moduleId: module.id,
-    templateUrl:`org.component.html`
-})
-export class OrgComponent  { 
-    path:string = 'resources/workplace/04 - orgChart.jpg';
-}
