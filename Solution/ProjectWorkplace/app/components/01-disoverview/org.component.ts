9b5450dda674d6b3a8762a448c5138fb6f14eeea
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-05-12 14:30:59 -0400

    up

--- Solution/ProjectWorkplace/app/components/01-disoverview/org.component.ts
+++ Solution/ProjectWorkplace/app/components/01-disoverview/org.component.ts
@@ -5,5 +5,19 @@ import { Component } from '@angular/core';
 })
 export class OrgComponent  { 
     path:string = 'resources/workplace/01DIS OVERVIEW/02-Organizational Chart.jpg';
-    breadcrumbs =['DIS Overview' ,'Organizational Structure'];
+    breadcrumbs =['DIS Overview' ,'Organizational Structure'];slides: any[] = [];
+
+    public constructor() {
+        // this.addSlide('ServiceCatalogue_GlobalReportAndDigitalStrategy.png');
+        // this.addSlide('ServiceCatalogue_ISOps.png');
+        // this.addSlide('ServiceCatalogue_SocialMediaServices.png');
+        
+        this.addSlide('02-Organizational Chart.jpg');
+    }
+
+    addSlide(filename:string): void {
+        this.slides.push({
+            image: `resources/workplace/01DIS OVERVIEW/${ filename }`
+        });
+    }
 }
